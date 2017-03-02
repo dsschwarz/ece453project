556ff4a30f760aa9e896295b16439b3dd7589e1f
dsschwarz <dan-schwarz@hotmail.com>
2017-03-02 13:59:28 -0500

    Added Parser skeleton

---
+++ pi/src/Parser.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Parser {
+    // createKey(functionName, functionName) -> #times functions are called in the same scope
+    public Dictionary<String, Integer> functionPairCount = new Hashtable<String, Integer>() {};
+    // functionName -> #time function is called
+    public Dictionary<String, Integer> functionCounts = new Hashtable<String, Integer>();
+
+    /**
+     * Parse a callgraph file
+     * @param fileName
+     */
+    public void parseFile(String fileName) {
+
+    }
+
+    /**
+     * Parse a single scope
+     */
+    private void parseScope() {
+
+    }
+
+    /**
+     * Creates a key from two function names. (name1, name2) generates the same name as (name2, name1)
+     * @return
+     */
+    static public String createKey(String name1, String name2) {
+        String key;
+        if (name1.compareTo(name2) < 0) {
+            key = name2 + ":" + name1;
+        } else {
+            key = name1 + ":" + name2;
+        }
+        return key;
+    }
+
+    private void recordFunctionPair(String name1, String name2) {
+        String key = createKey(name1, name2);
+
+        Integer current = functionPairCount.get(key);
+        if (current == null) {
+            functionPairCount.put(key, 1); // not recorded yet
+        } else {
+            functionPairCount.put(key, current + 1); // increment the count
+        }
+    }
+}
